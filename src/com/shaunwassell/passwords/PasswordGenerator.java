6585454fffd4b22dc4d376a93e5529d1a34a5b23
shaunwa <shaun.p.wassell@gmail.com>
2024-09-16 14:29:02 -0700

    Added other character sets

--- src/com/shaunwassell/passwords/PasswordGenerator.java
+++ src/com/shaunwassell/passwords/PasswordGenerator.java
@@ -6,6 +6,10 @@ public class PasswordGenerator {
     String generatePassword(int length, boolean useLowercase, boolean useUppercase,
                             boolean useNumbers, boolean useSpecialChars) {
         String lowercase = "abcdefghijklmnopqrstuvwxyz";
+        String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String digits = "0123456789";
+        String symbols = "!@#$%^&*()?";
+
         StringBuilder password = new StringBuilder();
         Random rand = new Random();
 
