e03cdecdb0cc4e821641dc95ca895543246bacf7
shaunwa <shaun.p.wassell@gmail.com>
2024-09-16 12:43:58 -0700

    Added logic for accepting and processing guesses

--- src/com/shaunwassell/wordgame/WordGuessingGame.java
+++ src/com/shaunwassell/wordgame/WordGuessingGame.java
@@ -26,7 +26,25 @@ public class WordGuessingGame {
             System.out.println("You have " + attempts + " attempts left...");
             System.out.print("Guess a letter: ");
 
-            attempts--;
+            char guess = scanner.nextLine().toLowerCase().charAt(0);
+
+            if (processGuess(guess)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("That guess was incorrect!");
+                attempts--;
+            }
         }
+    }
+
+    private boolean processGuess(char letter) {
+        boolean letterFound = false;
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            if (wordToGuess.charAt(i) == letter) {
+                guessedLetters[i] = letter;
+                letterFound = true;
+            }
+        }
+        return letterFound;
     }
 }
