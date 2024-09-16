9216ed44d1dba0397b58b92c56e5cb64587b99ab
shaunwa <shaun.p.wassell@gmail.com>
2024-09-16 14:08:27 -0700

    Added a text-based interface

---
+++ src/com/shaunwassell/passwords/TextBasedInterface.java
@@ -0,0 +1,46 @@
+package com.shaunwassell.passwords;
+
+import java.util.Scanner;
+
+public class TextBasedInterface implements UserInterface {
+    private Scanner scanner;
+
+    public TextBasedInterface() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public int getPasswordLength() {
+        System.out.print("Please enter the password length: ");
+        return scanner.nextInt();
+    }
+
+    @Override
+    public boolean useLowercase() {
+        System.out.print("Would you like the password to have lowercase characters? (y/n): ");
+        return scanner.next().equalsIgnoreCase("y");
+    }
+
+    @Override
+    public boolean useUppercase() {
+        System.out.print("Would you like the password to have uppercase characters? (y/n): ");
+        return scanner.next().equalsIgnoreCase("y");
+    }
+
+    @Override
+    public boolean useNumbers() {
+        System.out.print("Would you like the password to have numerical characters? (y/n): ");
+        return scanner.next().equalsIgnoreCase("y");
+    }
+
+    @Override
+    public boolean useSymbols() {
+        System.out.print("Would you like the password to have symbol characters? (y/n): ");
+        return scanner.next().equalsIgnoreCase("y");
+    }
+
+    @Override
+    public void displayPassword(String password) {
+        System.out.println("Here is the password: " + password);
+    }
+}
