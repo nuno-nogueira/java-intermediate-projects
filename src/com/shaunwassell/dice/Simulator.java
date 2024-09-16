4c0fc0f48b71ca63b1ffc6dba729f204de8e42c2
shaunwa <shaun.p.wassell@gmail.com>
2024-09-16 11:33:10 -0700

    Added die faces

--- src/com/shaunwassell/dice/Simulator.java
+++ src/com/shaunwassell/dice/Simulator.java
@@ -6,6 +6,19 @@ public class Simulator {
     public static void main(String[] args) {
         Random rand = new Random();
 
-        System.out.println(rand.nextInt(6) + 1);
+        int rolledNumber = rand.nextInt(6) + 1;
+        System.out.print(display(rolledNumber));
+    }
+
+    static String display(int value) {
+        switch (value) {
+            case 1: return "---------\n|       |\n|   o   |\n|       |\n---------";
+            case 2: return "---------\n| o     |\n|       |\n|     o |\n---------";
+            case 3: return "---------\n| o     |\n|   o   |\n|     o |\n---------";
+            case 4: return "---------\n| o   o |\n|       |\n| o   o |\n---------";
+            case 5: return "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
+            case 6: return "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
+            default: return "Not a valid die value";
+        }
     }
 }
