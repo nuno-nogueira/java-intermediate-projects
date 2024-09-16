0700c5e8db7ca575cbbd930d965a229e25723ca3
shaunwa <shaun.p.wassell@gmail.com>
2024-09-16 11:52:50 -0700

    Rolled multiple dice

--- src/com/shaunwassell/dice/Simulator.java
+++ src/com/shaunwassell/dice/Simulator.java
@@ -2,31 +2,38 @@ package com.shaunwassell.dice;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Simulator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How many dice would you like to roll?");
-        int numberOfDice = scanner.nextInt();
+        try {
+            System.out.println("How many dice would you like to roll?");
+            int numberOfDice = scanner.nextInt();
 
-        System.out.println("About to roll " + numberOfDice + " dice");
+            System.out.println("About to roll " + numberOfDice + " dice");
 
-//        Random rand = new Random();
-//
-//        int rolledNumber = rand.nextInt(6) + 1;
-//        System.out.print(display(rolledNumber));
+            Random rand = new Random();
+
+            for (int i = 0; i < numberOfDice; i++) {
+                int rolledNumber = rand.nextInt(6) + 1;
+                System.out.println(display(rolledNumber));
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("That is not a valid number!");
+        }
     }
 
     static String display(int value) {
-        switch (value) {
-            case 1: return "---------\n|       |\n|   o   |\n|       |\n---------";
-            case 2: return "---------\n| o     |\n|       |\n|     o |\n---------";
-            case 3: return "---------\n| o     |\n|   o   |\n|     o |\n---------";
-            case 4: return "---------\n| o   o |\n|       |\n| o   o |\n---------";
-            case 5: return "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
-            case 6: return "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
-            default: return "Not a valid die value";
-        }
+        return switch (value) {
+            case 1 -> "---------\n|       |\n|   o   |\n|       |\n---------";
+            case 2 -> "---------\n| o     |\n|       |\n|     o |\n---------";
+            case 3 -> "---------\n| o     |\n|   o   |\n|     o |\n---------";
+            case 4 -> "---------\n| o   o |\n|       |\n| o   o |\n---------";
+            case 5 -> "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
+            case 6 -> "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
+            default -> "Not a valid die value";
+        };
     }
 }
