af7f8eae430058602dc3daa2189741ec37bbc8cf
shaunwa <shaun.p.wassell@gmail.com>
2024-09-17 11:35:10 -0700

    Implemented the listFiles method

--- src/com/shaunwassell/filemanager/FileManager.java
+++ src/com/shaunwassell/filemanager/FileManager.java
@@ -1,8 +1,27 @@
 package com.shaunwassell.filemanager;
 
+import java.io.File;
+
 public class FileManager {
     public void listFiles(String directoryPath) {
-        System.out.println("Not implemented yet!");
+        File directory = new File(directoryPath);
+        if (directory.exists() && directory.isDirectory()) {
+            File[] contents = directory.listFiles();
+
+            if (contents != null) {
+                for (File file : contents) {
+                    if (file.isDirectory()) {
+                        System.out.println("[DIR]: " + file.getName());
+                    } else {
+                        System.out.println("[FILE]: " + file.getName());
+                    }
+                }
+            } else {
+                System.out.println("Directory is empty.");
+            }
+        } else {
+            System.out.println("Invalid directory path: Path either doesn't exist or is not a directory.");
+        }
     }
 
     public void createDirectory(String directoryPath) {
