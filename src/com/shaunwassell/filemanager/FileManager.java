5edf43caef76556b9b282d7e610fb7bce755caa3
shaunwa <shaun.p.wassell@gmail.com>
2024-09-17 11:24:05 -0700

    Added a file mgr class

---
+++ src/com/shaunwassell/filemanager/FileManager.java
@@ -0,0 +1,15 @@
+package com.shaunwassell.filemanager;
+
+public class FileManager {
+    public void listFiles(String directoryPath) {
+        System.out.println("Not implemented yet!");
+    }
+
+    public void createDirectory(String directoryPath) {
+        System.out.println("Not implemented yet!");
+    }
+
+    public void deleteFileOrDirectory(String directoryOrFilePath) {
+        System.out.println("Not implemented yet!");
+    }
+}
