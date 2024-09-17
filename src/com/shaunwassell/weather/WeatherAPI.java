ee555c34f0080687ed4eeedd95a9e02fa9565cd7
shaunwa <shaun.p.wassell@gmail.com>
2024-09-17 14:07:53 -0700

    Removed API Key

--- src/com/shaunwassell/weather/WeatherAPI.java
+++ src/com/shaunwassell/weather/WeatherAPI.java
@@ -9,7 +9,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class WeatherAPI {
-    private static final String API_KEY = "9f7c0fab007b8ea68c21321f2acb3853";
+    private static final String API_KEY = "YOUR API KEY HERE";
     private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
 
     public String getForecast(String city) throws IOException {
