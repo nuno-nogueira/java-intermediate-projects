e0cd333252a7618f2ebf2f7855210c7474cf0646
shaunwa <shaun.p.wassell@gmail.com>
2024-09-17 13:03:00 -0700

    Created a class for the weather API

---
+++ src/com/shaunwassell/weather/WeatherAPI.java
@@ -0,0 +1,6 @@
+package com.shaunwassell.weather;
+
+public class WeatherAPI {
+    private static final String API_KEY = "9f7c0fab007b8ea68c21321f2acb3853";
+    private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
+}
