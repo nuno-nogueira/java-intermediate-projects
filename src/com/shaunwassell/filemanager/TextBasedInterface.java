829e5966e19aa5451add8428d4d5951ac6f26b86
shaunwa <shaun.p.wassell@gmail.com>
2024-09-17 12:13:57 -0700

    Created the text-based user interface

--- src/com/shaunwassell/filemanager/TextBasedInterface.java
+++ src/com/shaunwassell/filemanager/TextBasedInterface.java
@@ -1,11 +1,44 @@
 package com.shaunwassell.filemanager;
 
+import java.util.Scanner;
+
 public class TextBasedInterface implements UserInterface {
     private UIEventHandler handler = null;
 
     @Override
     public void start() {
+        System.out.println("File Manager Menu:");
+        System.out.println("1. List files");
+        System.out.println("2. Create a directory");
+        System.out.println("3. Delete a file or directory");
+        System.out.println("4. Exit program");
+        System.out.println("Enter your choice: ");
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter directory path: ");
+                    handler.onList(scanner.nextLine());
+                    break;
+                case 2:
+                    System.out.println("Enter path of new directory: ");
+                    handler.onCreate(scanner.nextLine());
+                    break;
+                case 3:
+                    System.out.println("Enter path of file or directory to delete: ");
+                    handler.onDelete(scanner.nextLine());
+                case 4:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Not a valid choice!");
+            }
+        }
     }
 
     @Override
