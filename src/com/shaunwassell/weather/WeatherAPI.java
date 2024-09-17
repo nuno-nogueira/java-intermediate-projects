81e691a4f5a7b2036d8a7211107def152298c456
shaunwa <shaun.p.wassell@gmail.com>
2024-09-17 13:50:37 -0700

    Got user input for the city

--- src/com/shaunwassell/weather/WeatherAPI.java
+++ src/com/shaunwassell/weather/WeatherAPI.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class WeatherAPI {
     private static final String API_KEY = "9f7c0fab007b8ea68c21321f2acb3853";
     private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
 
     public String getForecast(String city) throws IOException {
-        String apiUrl = BASE_URL + "?q=" + city + "&appid=" + API_KEY + "&units=metric";
+        String apiUrl = BASE_URL + "?q=" + URLEncoder.encode(city, StandardCharsets.UTF_8.toString()) + "&appid=" + API_KEY + "&units=metric";
         URL url = new URL(apiUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
